864f27cbeec8788e18a7e5b27fd692cef3dbd6ce
marhc <marciohbc@gmail.com>
2019-04-22 17:10:41 -0300

    Configuring Babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
-    target: 'node'
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ["add-module-exports"]
+                    }
+                }
+            }
+        ]
+    }
 };
