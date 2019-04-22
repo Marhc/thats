58951fc633e8d37425089da169b1a1ab0c3b454d
marhc <marciohbc@gmail.com>
2019-04-22 10:11:50 -0300

    Configuring Webpack dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
- const path = require('path');
- const glob_entries = require('webpack-glob-entries');
- 
- module.exports = {
-   entry: glob_entries(path.resolve(__dirname,'src/**/*.js')),
-   output: {
-     path: path.resolve(__dirname, 'dist'),
-     filename: '[name].js'
-   },
-   target: 'node'
- };
+const path = require('path');
+const glob = require('glob');
+
+const entryArray = glob.sync(__dirname + '/src/**/index.js');
+
+const entryObject = entryArray.reduce((acc, item) => {
+  const name = item.replace('.js', '').replace(__dirname + '/src/', '');
+  acc[name] = item;
+  return acc;
+}, {});
+
+module.exports = {
+    entry: entryObject,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    target: 'node'
+};
